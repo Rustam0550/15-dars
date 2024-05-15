5253b4f0fe44871bd71e14e0c38f6933c2404942
Your Name <you@example.com>
2024-05-15 19:17:11 +0500

    comment_nomi

---
+++ skript.js
@@ -0,0 +1,13 @@
+// // alert("Muhammad");
+// // alert(15);
+
+// let username = prompt("ismizni kiriting")
+// let age = prompt("Yoshizni kiriting");
+// let address = prompt("Manzil kiriting");
+
+// alert("ismi Rustam" + username + "va yoshi" + age + "da");
+
+let num1 = +prompt("Birinchi sonni kiriting");
+let num2 = +prompt("ikkinci sonni kiriting");
+let num3 = +prompt("Uchinci sonni kiriting");
+alert(num1+num2);
